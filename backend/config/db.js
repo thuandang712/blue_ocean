179ededed7ad9de681bb8c14e594201af8efa101
thuandang712 <thuan71294@gmail.com>
2021-09-30 15:10:14 -0500

    td fixing security issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose")
-const config = require("config")
-const db = config.get("mongoURI")
+// const config = require("config")
+// const db = config.get("mongoURI")
+
+require('dotenv').config()
+const db = process.env.mongoURI
 
 // Create db connection function USING ASYNC AWAIT
 const connectDB = async () => {
