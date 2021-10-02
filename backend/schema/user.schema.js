9590b6d2c9c78c35e539924e6d0ea0c04e8a8db4
thuandang712 <thuan71294@gmail.com>
2021-10-01 22:36:57 -0500

    td commit changes on backend: added get and post routes to user

--- backend/schema/user.schema.js
+++ backend/schema/user.schema.js
@@ -4,8 +4,8 @@ const userSchema = new Schema({
     first_name: { type: String, maxLength: 20, required: true },
     last_name: { type: String, maxLength: 20, required: true },
     phone_number: { type: Number, maxLength: 11 },
-    email: { type: String, maxLength: 50, required: true },
-    password: { type: String, minLength: 8, maxLength: 20, required: true },
+    email: { type: String, maxLength: 50, required: true, unique: true },
+    password: { type: String, minLength: 8, maxLength: 100, required: true },
 });
 
 module.exports = {
