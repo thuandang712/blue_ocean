0328e5644ae1e5f0c5862b61954a6d453c752155
thuandang712 <thuan71294@gmail.com>
2021-10-17 18:03:34 -0500

    td finishing up the functionalities

--- client/src/components/ticket/SingleTicket.js
+++ client/src/components/ticket/SingleTicket.js
@@ -1,112 +1,206 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Row, Col, Button, Card, ListGroup } from "react-bootstrap";
-import { fetchSingleTicket } from '../../api/ticket.api';
+import React from 'react'
+import { Container, Form, Jumbotron, Row, Col, Button, Alert } from "react-bootstrap";
+
+import { updateSingleTicket, fetchSingleTicket } from '../../api/ticket.api';
 import { fetchTech } from '../../api/tech.api';
-import { useParams } from "react-router-dom";
-import AssignTech from '../tech/AssignTech';
+import DefaultLayout from '../../layout/DefaultLayout';
 
-const SingleTicket = () => {
-    const { _id } = useParams()
-    const [singleTicket, setSingleTicket] = useState({})
-    const [techs, setTechs] = useState([])
+const priorities = ['Low', 'Medium', 'High'];
+const statuses = ['Open', 'In Progress', 'Resolved'];
+const types = ['Bug/Error', 'Feature Request', 'Security', 'Other'];
 
 
-    useEffect(() => {
-        async function getData() {
-            try {
-                const res = await fetchSingleTicket(_id)
-                return res
-            } catch (error) {
-                console.log(error)
+class SingleTicket extends React.Component {
+
+    state = {
+        subject: '',
+        description: '',
+        assignee: '',
+        priority: '',
+        status: '',
+        type: '',
+        techs: [],
+        loading: false,
+        server_status: '',
+        message: ''
+    }
+
+
+    async componentDidMount() {
+        const { _id } = this.props.match.params;
+        this.setState({ loading: true })
+        // default state of ticket
+        const res = await fetchSingleTicket(_id)
+        this.setState({
+            subject: res.ticket.subject,
+            description: res.ticket.description,
+            assignee: res.ticket.assignee,
+            priority: res.ticket.priority,
+            status: res.ticket.status,
+            type: res.ticket.type
+        })
+        // get techs list
+        const { tech } = await fetchTech()
+        this.setState({ techs: tech })
+        this.setState({ loading: false })
+    }
+
+    render() {
+        const { _id } = this.props.match.params;
+        const { subject, description, assignee, priority, status, type, message, techs, server_status } = this.state
+
+        const handleOnChange = (e) => {
+            const key = e.target.name
+            const value = e.target.value
+            this.setState({ [key]: value })
+        }
+
+        const handleOnSubmit = async (e) => {
+            e.preventDefault();
+
+            const ticketObj = { subject, description, assignee, priority, status, type } // key has to match with db
+
+            this.setState({ loading: true })
+
+            // PATCH request to /api/ticket/:id
+            const resData = await updateSingleTicket(_id, ticketObj)
+
+            if (resData.status === "success") {
+                this.setState({ loading: false, server_status: 'success', message: resData.message })
+            } else {
+                this.setState({ loading: false, server_status: 'error', message: resData.message })
             }
         }
 
-        getData().then((data) => {
-            setSingleTicket(data.ticket)
-        })
-
-    }, [_id])
 
 
-    const handleAssign = async () => {
-        // populate all tech with 
-        const res = await fetchTech()
-        setTechs(res.tech)
+        return (
+            <DefaultLayout>
+                <Container>
+                    <Jumbotron className="mt-3 add-new-ticket bg-light">
+                        <Row>
+                            <Col>
+                                <h1 className="text-info text-center mb-2">Edit Ticket</h1>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col>
+                                {message && (
+                                    <Alert variant={server_status === "success" ? "success" : "danger"}>
+                                        {message}
+                                    </Alert>
+                                )}
+                            </Col>
+                        </Row>
+
+                        <Form autoComplete="off" onSubmit={handleOnSubmit}>
+
+                            <Form.Group as={Row}>
+                                <Form.Label column sm={3}>Subject</Form.Label>
+                                <Col sm={9}>
+                                    <Form.Control
+                                        name="subject"
+                                        value={subject}
+                                        maxLength="200"
+                                        onChange={handleOnChange}
+                                        placeholder="Subject"
+                                        required
+                                    />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row}>
+                                <Form.Label column sm={3}>Description</Form.Label>
+                                <Col sm={9}>
+                                    <Form.Control
+                                        name="description"
+                                        value={description}
+                                        maxLength="1000"
+                                        onChange={handleOnChange}
+                                        placeholder="Description"
+                                        required
+                                    />
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row}>
+                                <Form.Label column sm={3}>Assign To</Form.Label>
+                                <Col sm={9}>
+                                    <select className="form-control"
+                                        name='assignee'
+                                        value={assignee}
+                                        onChange={handleOnChange}>
+                                        {techs.map(tech => {
+                                            return <option key={tech._id} value={`${tech.first_name} ${tech.last_name}`}>
+                                                {`${tech.first_name} ${tech.last_name}`}
+                                            </option>
+                                        })}
+                                    </select>
+                                </Col>
+                            </Form.Group>
+
+                            <Form.Group as={Row}>
+                                <Form.Label column sm={3}>Priority</Form.Label>
+                                <Col sm={9}>
+                                    <select className="form-control"
+                                        name='priority'
+                                        value={priority}
+                                        onChange={handleOnChange}>
+                                        {priorities.map((priority, index) => {
+                                            return <option key={index} value={priority}>
+                                                {priority}
+                                            </option>
+                                        })}
+                                    </select>
+                                </Col>
+                            </Form.Group>
 
 
+                            <Form.Group as={Row}>
+                                <Form.Label column sm={3}>Status</Form.Label>
+                                <Col sm={9}>
+                                    <select className="form-control"
+                                        name='status'
+                                        value={status}
+                                        onChange={handleOnChange}>
+                                        {statuses.map((status, index) => {
+                                            return <option key={index} value={status}>
+                                                {status}
+                                            </option>
+                                        })}
+                                    </select>
+                                </Col>
+                            </Form.Group>
 
 
-        // add tickets to tech 
-        // get ticket id 
-        // find by ID and update  ticket  { tech: tech._id }
+                            <Form.Group as={Row}>
+                                <Form.Label column sm={3}>Type</Form.Label>
+                                <Col sm={9}>
+                                    <select className="form-control"
+                                        name='type'
+                                        value={type}
+                                        onChange={handleOnChange}>
+                                        {types.map((type, index) => {
+                                            return <option key={index} value={type}>
+                                                {type}
+                                            </option>
+                                        })}
+                                    </select>
+                                </Col>
+                            </Form.Group>
 
+                            <Button type="submit" variant="info" block>
+                                Update Ticket
+                            </Button>
+
+                        </Form>
+                    </Jumbotron >
+                </Container >
+            </DefaultLayout>
+
+        )
     }
-
-
-    const assignTicketToTech = async (techId) => {
-        // const ticketId = _id
-
-
-        // fetch single tech 
-        // const { tech } = await fetchSingleTech(techId)
-        // tech.tickets.push(ticketId)
-        // const obj = { ticketId: ticketId, status: 'Working' }
-        // console.log(obj)
-        // // find by ID and update tech schema
-        // const res = await updateTicketArrayInTech(techId, obj)
-        // console.log(res)
-        // call api to this single tech
-    }
-
-
-
-    return (
-
-        <Container>
-            <Row>
-                <Col>
-                    <h1>Single Ticket Page</h1>
-                </Col>
-            </Row>
-
-
-            <Row>
-                <Col className="text-weight-bolder text-secondary">
-
-                    <Card>
-                        <ListGroup variant="flush">
-                            <ListGroup.Item id={singleTicket._id}>Subject: {`${singleTicket.subject}`}</ListGroup.Item>
-                            <ListGroup.Item>Sender: {singleTicket.sender}</ListGroup.Item>
-                            <ListGroup.Item>Status: {singleTicket.status}</ListGroup.Item>
-                            <ListGroup.Item>Message: {singleTicket.message}</ListGroup.Item>
-                        </ListGroup>
-                        {/* <Button className='delete-btn w-50' size='sm' variant="outline-danger"
-                                id={tech._id} onClick={handleDelete}>Delete
-                            </Button> */}
-                    </Card>
-
-                </Col>
-
-
-                <Col className="text-right">
-                    <Button variant="outline-info"
-                        onClick={handleAssign}
-                    >Assign</Button>
-                </Col>
-            </Row>
-
-            <hr />
-
-
-            <Row xs={1} md={3} className="g-5 mt-2">
-                {techs.map(tech => (
-                    <AssignTech key={tech._id} tech={tech} assignTicketToTech={assignTicketToTech} />
-                ))}
-            </Row>
-        </Container>
-
-    )
-
 }
 
 
